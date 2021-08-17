ec69b829b63bbe362cf959309473e69f4947140c
monaSalih <monaalobisat@gmail.com>
2021-08-17 17:17:14 +0300

    Api work

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-'use strict'
 
-const express=require('express');
+const express = require('express');
 require('dotenv').config();
-const server=express();
 const cors = require('cors')
-const brodCast=require('./assets/weather.json')
-const PORT=process.env.PORT;
-
+const brodCast = require('./assets/weather.json')
+const server = express();
+const PORT = process.env.PORT;
 server.use(cors());
 
 
 
-//localhost:3001/wetherCast?reqName=venusaur
-server.get('/wetherCast',(req,res)=>{
- console.log(req.query);
- let locaName=req.query.reqName  
- let wetherData=brodCast.results.find(info=>{
-        if (info.city_name.toLowerCase()===locaName){
-            return info
+
+
+//localhost:3001/wetherCast?info=namede
+server.get('/wetherCast', (req, res) => {
+    console.log(req.query,'info city');
+    let locaName = req.query.info;
+    console.log(brodCast,'brodCast info');
+    let wetherData = brodCast.find(info => {
+        // console.log(info.city_name,);
+        if (info.city_name.toLowerCase() === locaName.toLowerCase() ) {
+        console.log(locaName,'local name result');
+            return info;
         }
-        res.send(wetherData)
     })
+    res.send(wetherData);
+    // console.log(wetherData[0]);
 })
 
-server.listen(PORT,()=>{
-    console.log(`listning to port${PORT}`);
+server.get('*', (req, res) => {
+    res.status(404).send('There is an error try again next time ')
+})
+
+
+
+server.listen(PORT, () => {
+    console.log(`Listning on PORT ${PORT}`)
 })
