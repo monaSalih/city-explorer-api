48321111587f07f34b2cf2e97466d1559fb6b305
monaSalih <monaalobisat@gmail.com>
2021-08-18 18:19:41 +0300

    lab09Modul

--- server.js
+++ server.js
@@ -2,68 +2,49 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors')
-const brodCast = require('./assets/weather.json')
+// const brodCast = require('./assets/weather.json')
 const server = express();
 const PORT = process.env.PORT;
 server.use(cors());
 const axios = require('axios')
 
-server.get('/photo',MoviePlayInCity)
+// server.get('/photo', MoviePlayInCity)
+
+
+// class cityWeatherInfo {
+//     constructor(index) {
+//         this.date = index.valid_date;
+//         this.description = index.weather.description
+//     }
+// }
+
+// class showResultImage {
+//     constructor(dataStore) {
+//         this.movImage = dataStore.original_title
+//         this.title = dataStore.original_title
+//         this.moviePoster = `https://image.tmdb.org/t/p/w500${dataStore.poster_path}`
+
+//     }
+// }
 
 //localhost:3001/wetherCast?info=namede
-server.get('/wetherCast', (req, res) => {
-    console.log(req.query,'info city');
-    let locaName = req.query.info;
-    console.log(brodCast,'brodCast info');
-    let wetherData = brodCast.find(info => {
-        // console.log(info.city_name,);
-        if (info.city_name.toLowerCase() === locaName) {
-        console.log(locaName,'local name result');
-            return info;
-        }
+const getWeatherModul=require("./assets/weather")
+server.get('/wetherCast',getWeatherModul)
+
+//////////localhost:3001/movies?queryCity=
+const getMovieModul=require("./assets/movie")
+server.get('/movies',getMovieModul)
+    // https://api.themoviedb.org/3/search/movie?api_key=0c627e9465b11099d881815e4147eebb&query=seattle
+ 
+        
+            
+      
+
+
+
+    server.listen(PORT, () => {
+        console.log(`Listning on PORT ${PORT}`)
     })
-    res.send(wetherData);
-    // console.log(wetherData[0]);
-    ///test
-})
-///////////////////////////////////////////movie show in slected city
-  // https://api.themoviedb.org/3/search/movie?api_key=0c627e9465b11099d881815e4147eebb&query=seattle
-
-//////////localhost:3001/movieOnCity?info=
- function  MoviePlayInCity(req, res){
-let photoSlectRe=req.query.queryCity;
-let url=` https://api.themoviedb.org/3/search/movie?api_key=${process.env.CITY_MOVIE}&query=${photoSlectRe} `
-try
-{
-  
-     axios.get(url).then((mapResult)=>{
-    console.log(movieData.data);
-let selectedMovie=mapResult.data.map(imgWl=>{
-    return new classmovie(imgWl)
-})
-
-res.send(selectedMovie)
-})
-    }
-catch(error){
-
-    res.send(error)
-}
-}
-class showResultImage{
-    constructor (dataStore){
-        this.movImage=dataStore.original_title
-        this.title=dataStore.original_title
-        this.moviePoster=dataStore.poster_path
-
-    }
-}
- 
-
- 
-server.listen(PORT, () => {
-    console.log(`Listning on PORT ${PORT}`)
-})
-server.get('*', (req, res) => {
-    res.status(404).send('There is an error try again next time ')
-})
+    server.get('*', (req, res) => {
+        res.status(404).send('There is an error try again next time ')
+    })
